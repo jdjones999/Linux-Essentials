83d8a47b4f48bf5450ee479676ef31b6bd05e87c
jdjones999 <jdjones999@gmail.com>
2024-03-27 13:15:40 -0700

    Update install_search.sh

--- install_search.sh
+++ install_search.sh
@@ -7,14 +7,23 @@ NC='\033[0m' # No Color
 # Function to display section header
 display_section_header() {
     printf "\n${RED}$1${NC}\n\n"
-    
+}
+
 # Check if the current user is root
 if [ "$(id -u)" = "0" ]; then
     # If user is root, no need for sudo
-    cp ~/search /bin/search
-    display_section_header "Search has been installed successfully."
+    if [ -f "/bin/search" ]; then
+        display_section_header "Search has been previously installed."
+    else
+        cp ~/search /bin/search
+        display_section_header "Search has been installed successfully."
+    fi
 else
     # If user is not root, use sudo
-    sudo cp ~/search /bin/search
-    display_section_header "Search has been installed successfully."
+    if [ -f "/bin/search" ]; then
+        display_section_header "Search has been previously installed."
+    else
+        sudo cp ~/search /bin/search
+        display_section_header "Search has been installed successfully."
+    fi
 fi
