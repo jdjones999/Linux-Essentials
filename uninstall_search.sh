00d2f5779ec7d3ccb88bd2a241b126a00fb409fb
jdjones999 <jdjones999@gmail.com>
2024-03-27 13:02:18 -0700

    Update uninstall_search.sh
    Added color to comment

--- uninstall_search.sh
+++ uninstall_search.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Color codes
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Function to display section header
+display_section_header() {
+    printf "\n${RED}$1${NC}\n\n"
+    
 # Check if the current user is root
 if [ "$(id -u)" = "0" ]; then
     # If user is root, no need for sudo
     rm /bin/search
-    echo "Search has been uninstalled successfully."
+    display_section_header "Search has been uninstalled successfully."
 else
     # If user is not root, use sudo
     sudo rm /bin/search
-    echo "Search has been uninstalled successfully."
+    display_section_header "Search has been uninstalled successfully."
 fi
