9b0f05c3848b7c5dfc12b1934ec2266c0768e306
jdjones999 <jdjones999@gmail.com>
2024-03-27 12:52:41 -0700

    Create install_search.sh
    Install "search" to run as a command globally.

---
+++ install_search.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Color codes
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Function to display section header
+display_section_header() {
+    printf "\n${RED}$1${NC}\n\n"
+    
+# Check if the current user is root
+if [ "$(id -u)" = "0" ]; then
+    # If user is root, no need for sudo
+    cp ~/search /bin/search
+    display_section_header "Search has been installed successfully."
+else
+    # If user is not root, use sudo
+    sudo cp ~/search /bin/search
+    display_section_header "Search has been installed successfully."
+fi
