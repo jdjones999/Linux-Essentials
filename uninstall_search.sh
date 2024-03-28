9ef84a8e0ca9610ad38d6555581ea5b35f218bd1
jdjones999 <jdjones999@gmail.com>
2024-03-28 14:39:12 -0700

    Update uninstall_search.sh

--- uninstall_search.sh
+++ uninstall_search.sh
@@ -7,14 +7,23 @@ NC='\033[0m' # No Color
 # Function to display section header
 display_section_header() {
     printf "\n${RED}$1${NC}\n\n"
-    
+}
+
 # Check if the current user is root
 if [ "$(id -u)" = "0" ]; then
     # If user is root, no need for sudo
-    rm /bin/search
-    display_section_header "Search has been uninstalled successfully."
+    if [ -f "/bin/search" ]; then
+        rm /bin/search
+        display_section_header "Search has been uninstalled successfully."
+    else
+        display_section_header "Search is not installed."
+    fi
 else
     # If user is not root, use sudo
-    sudo rm /bin/search
-    display_section_header "Search has been uninstalled successfully."
+    if [ -f "/bin/search" ]; then
+        sudo rm /bin/search
+        display_section_header "Search has been uninstalled successfully."
+    else
+        display_section_header "Search is not installed."
+    fi
 fi
