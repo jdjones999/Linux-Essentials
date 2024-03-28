56a6f7d379a986f2d0f00d61ae5e2f04820f2d92
Jermaine Jones <jdjones999@gmail.com>
2024-03-28 14:57:05 -0700

    Add install_search.sh script

--- install_search.sh
+++ install_search.sh
@@ -9,13 +9,16 @@ display_section_header() {
     printf "\n${RED}$1${NC}\n\n"
 }
 
+# Get the current directory of the script
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
 # Check if the current user is root
 if [ "$(id -u)" = "0" ]; then
     # If user is root, no need for sudo
     if [ -f "/bin/search" ]; then
         display_section_header "Search has been previously installed."
     else
-        cp ~/search /bin/search
+        cp "$SCRIPT_DIR/search" /bin/search
         display_section_header "Search has been installed successfully."
     fi
 else
@@ -23,7 +26,7 @@ else
     if [ -f "/bin/search" ]; then
         display_section_header "Search has been previously installed."
     else
-        sudo cp ~/search /bin/search
+        sudo cp "$SCRIPT_DIR/search" /bin/search
         display_section_header "Search has been installed successfully."
     fi
 fi
