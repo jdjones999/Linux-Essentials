d2f1cb0f340024d0163a7240e7cbdfa1e92a4cc5
jdjones999 <jdjones999@gmail.com>
2024-03-31 20:50:12 -0700

    Update install_v.01.sh

--- install_v.01.sh
+++ install_v.01.sh
@@ -25,7 +25,81 @@ if [ -f "requirements.txt" ]; then
         display_section_header "$GREEN" "Installed $package"
     done < requirements.txt
 
-    display_section_header "$GREEN" "Installation complete."
+    display_section_header "$GREEN" "Requirements installation complete."
 else
     display_section_header "$RED" "Error: requirements.txt file not found."
+fi
+#===============================[ Install (search) ]===============================
+
+# Get the current directory of the script
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Check if the current user is root
+if [ "$(id -u)" = "0" ]; then
+    # If user is root, no need for sudo
+    if [ -f "/bin/search" ]; then
+        display_section_header "$ORANGE" "Search has been previously installed."
+    else
+        cp "$SCRIPT_DIR/search" /bin/search
+        chmod +x /bin/search
+        display_section_header "$GREEN" "Search has been installed successfully."
+    fi
+else
+    # If user is not root, use sudo
+    if [ -f "/bin/search" ]; then
+        display_section_header "$ORANGE" "Search has been previously installed."
+    else
+        sudo cp "$SCRIPT_DIR/search" /bin/search
+        sudo chmod +x /bin/search
+        display_section_header "$GREEN" "Search has been installed successfully."
+    fi
+fi
+
+#===============================[ Install (banner) ]===============================
+
+# Specify the URL of the file to download
+url="http://www.figlet.org/fonts/rowancap.flf"
+
+# Specify the target directory
+target_dir="/usr/share/figlet"
+
+# Specify the filename
+filename="rowancap.flf"
+
+display_section_header "$GREEN" "Installing font for (banner)"
+
+# Check if the file already exists in the target directory
+if sudo [ ! -f "$target_dir/$filename" ]; then
+    # File doesn't exist, so download it
+    sudo wget -P "$target_dir" "$url"
+    display_section_header "$GREEN" "figlet font (rowancap) downloaded successfully."
+else
+    # File already exists
+    display_section_header "$ORANGE" "figlet font (rowancap) file already exists in the target directory."
+fi
+
+#==
+
+# Get the current directory of the script
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+# Check if the current user is root
+if [ "$(id -u)" = "0" ]; then
+    # If user is root, no need for sudo
+    if [ -f "/bin/banner" ]; then
+        display_section_header "$ORANGE" "banner has been previously installed."
+    else
+        cp "$SCRIPT_DIR/banner" /bin/banner
+        chmod +x /bin/banner
+        display_section_header "$GREEN" "banner has been installed successfully."
+    fi
+else
+    # If user is not root, use sudo
+    if [ -f "/bin/banner" ]; then
+        display_section_header "$ORANGE" "banner has been previously installed."
+    else
+        sudo cp "$SCRIPT_DIR/banner" /bin/banner
+        sudo chmod +x /bin/banner
+        display_section_header "$GREEN" "banner has been installed successfully."
+    fi
 fi
