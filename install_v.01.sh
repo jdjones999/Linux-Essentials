9e4fccc628eccc529683345e01951ec7695f1b2b
jdjones999 <jdjones999@gmail.com>
2024-04-03 14:44:33 -0700

    Update requirements

--- install_v.01.sh
+++ install_v.01.sh
@@ -13,21 +13,21 @@ BLUE='\033[0;34m'   # Blue color code
 GREEN='\033[38;2;22;196;12m'  # Green -Terminal Green
 ORANGE='\033[0;33m' # Orange color code
 NC='\033[0m' # No Color
-#===============================[ Install form requirements.conf ]===============================
-# Check if requirements.conf file exists
-if [ -f "requirements.conf" ]; then
-    display_section_header "$BLUE" "Installing packages from requirements.conf:"
+#===============================[ Install form requirements.txt ]===============================
+# Check if requirements.txt file exists
+if [ -f "requirements.txt" ]; then
+    display_section_header "$BLUE" "Installing packages from requirements.txt:"
 
-    # Read each line in requirements.conf and install the packages
+    # Read each line in requirements.txt and install the packages
     while IFS= read -r package; do
         display_section_header "$BLUE" "Installing $package..."
         sudo apt-get install -y "$package"
         display_section_header "$GREEN" "Installed $package"
-    done < requirements.conf
+    done < requirements.txt
 
     display_section_header "$GREEN" "Requirements installation complete."
 else
-    display_section_header "$RED" "Error: requirements.conf file not found."
+    display_section_header "$RED" "Error: requirements.txt file not found."
 fi
 #===============================[ Install (search) ]===============================
 
